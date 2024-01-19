493f28b7a7dc9cec4220ff9d4052c2b8a65edd5e
Adam Osorno <adamosorno@gmail.com>
2024-01-18 23:24:21 -0800

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,11 @@
+const myHeading = document.querySelector("h2");
+myHeading.textContent = total
+const houseMembers = 6;
+const houseSize = 7;
+const food = 14;
+const water = 5;
+const houseItem = 4;
+const waste = 50;
+const recycle = 8;
+const travel = 4;
+const total = houseMembers + houseSize + food + water + houseItem + waste + recycle + travel
