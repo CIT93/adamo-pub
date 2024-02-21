6a2262eca847ca2cd76a9c413b52eec8a3bbc513
Adam Osorno <adamosorno@gmail.com>
2024-02-20 22:43:09 -0800

    Setup done

--- w6/main.js
+++ w6/main.js
@@ -1,3 +1,5 @@
+const FORM = document.getElementById("form");
+const OUTPUT = document.getElementById("output");
 const cfpData = []
 
 function determineHouseHoldPts(numberInHousehold) {
@@ -43,96 +45,50 @@ function displayOutObj(obj) {
   newH3.textContent = 'based on the number in and size of your home'
   const newP2 = document.createElement("p");
   newP2.textContent = `${obj.hsPts} for your ${obj.houseS} home`;
-  output.appendChild(newP);
-  output.appendChild(newH3);
-  output.appendChild(newP2);
+  OUTPUT.appendChild(newP);
+  OUTPUT.appendChild(newH3);
+  OUTPUT.appendChild(newP2);
 }
 
 function start(houseMembers, houseSize) {
   const houseHoldPTS = determineHouseHoldPts(houseMembers);
   const houseSizePTS = calculateEP(houseSize);
   const total = houseHoldPTS + houseSizePTS;
-  const myObj2 = {
+  cfpData.push( {
     houseM: houseMembers,
     houseS: houseSize,
     hmPts: houseHoldPTS,
     hsPts: houseSizePTS,
     cfpTotal: total,
-  }
-  displayOutObj (myObj2);
-  //cfpData.push([houseMembers, houseSize, houseHoldPTS, houseSizePTS, total]);
+  });
+ 
 }
 
-//function displayOutput() {
-  //for (arr of cfpData){
-    //console.log(arr)
-    //const output = document.getElementById("output");
-    //const newP = document.createElement("h2");
-    //newP.textContent = `Carbon Footprint total is ${arr[4]}`;
-    //const newH3 = document.createElement("h3");
-    //newH3.textContent = 'based on the number in and size of your home'
-    //const newP2 = document.createElement("p");
-    //newP2.textContent = `${arr[3]} for your ${arr[1]} home`;
-    //output.appendChild(newP);
-    //output.appendChild(newH3);
-    //output.appendChild(newP2);
-  //}
-//}
+function displayOutput() {
+  for (obj of cfpData){
+    const newP = document.createElement("h2");
+    newP.textContent = `Carbon Footprint total ${obj.cfpTotal}`;
+    const newH3 = document.createElement("h3");
+    newH3.textContent = `based on the ${obj.houseM} people in your home`;
+    const newP2 = document.createElement("p");
+    newP2.textContent = `you have ${obj.hsPts} points for your ${obj.houseS} home`;
+    OUTPUT.appendChild(newP);
+    OUTPUT.appendChild(newH3);
+    OUTPUT.appendChild(newP2);
+  }
+}
 
-//function displayOutput() {
- // for (let i = 0; i < cfpData.length; i++){
-  //  console.log(i)
-   // const output = document.getElementById("output");
-   // const newP = document.createElement("h2");
-   // newP.textContent = `Carbon Footprint total is ${cfpData[i][4]}`;
-   // const newH3 = document.createElement("h3");
-   // newH3.textContent = 'based on the number in and size of your home'
-    //const newP2 = document.createElement("p");
-    //newP2.textContent = `${cfpData[3]} for your ${cfpData[1]} home`;
-    //output.appendChild(newP);
-    //output.appendChild(newH3);
-    //output.appendChild(newP2);
-  //}
-//}
-
-start(5,"apartment");
-start(4, "large");
-start(3,"medium");
-start(2,"small");
-start(6,"medium");
-start(8,"apartment");
-
+console.log(cfpData);
 displayOutput();
 
-//for (initialization; condition; afterthought)
-// statement
-
-//for (let i = 0; i < 5; i++) {
-  //block scope
- //console.log(i);
-//}
-
-//for (let i = 1; i < 16; i++) {
-  //block scope
- //console.log(i);
-//}
-
-//for (let i =6; i > -1; i--) {
-  //block scope
- //console.log(i);
-//}
-
-// Intro to objects
-
-const myArr = [];
-const myObj = {
-  cfpTotal: 18,
-  houseSize: "small",
-  displayOut: function(){
-    console.log("this is a method call");
-    console.log(this.houseSize);
-  }
-};
-//console.log(myObj.cfpTotal)
-//console.log(myObj["houseSize"])
-myObj.displayOut();
+FORM.addEventListener('submit', function(e){
+  e.preventDefault();
+  const firstName = FORM.firstname.value;
+  const lastName = FORM.lastname.value;
+  const houseMembers = FORM.housem.value;
+  const houseSize = FORM.houses.value;
+  start(houseMembers, houseSize);
+  OUTPUT.innerHTML="";
+  displayOutput();
+  FORM.reset();
+})
